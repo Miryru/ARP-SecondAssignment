5b59717925603bd1bbadfe2f8b3bb389df8249af
Ivanterry00 <97966280+Ivanterry00@users.noreply.github.com>
2023-01-12 22:51:22 +0100

    commenti, readme e info.txt

--- ARP-SecondAssignment/README.md
+++ ARP-SecondAssignment/README.md
@@ -1,14 +1,15 @@
-# ARP-Assignment2
-Base repository for the **second ARP assignment**.
+# Second Assignment for the course "Advanced and Robot Programming" - UniGe
+### Developed by  [@Miryru](https://github.com/Miryru), [@alemuraa](https://github.com/alemuraa), [@Ivanterry00](https://github.com/Ivanterry00)
+Assignment given by the professor Renato Zaccaria for the course "Advanced and Robot Programming" - Robotics Engineering, at the University of Genova.
+
 The project provides you with a base infrastructure for the implementation of the simulated vision system through shared memory, according to the requirements specified in the PDF file of the assignment.
 
-The two processes involved in the simulation of the vision system, namely **processA** and **processB**, are implemented as simple *ncurses windows*. The development of the inter-process communication pipeline, that is the shared memory, is left to you.
+###```Read the [info.txt] to know the info about the project.```
+-----
 
-As for the first assignment, you also find a **master** process already prepared for you, responsible of spawning the entire simulation.
+## Library installation and usage guide 
+To execute the program you need to install the *libbitmap* library. This library is used to read and write bitmap images. It is a shared library, which means that it is not linked to the executable at compile time, but rather at runtime. This allows the library to be updated without having to recompile the programs which use it.
 
-Additionally, I have prepared a simple program called **circle.c**, which shows you the basic functionalities of the *libbitmap* library. Please, note that the **circle.c** process must not appear in your final project. It is simply meant to be a guide for you on how to use the bitmap library, therefore you will need to properly use portions of that code in **processA** and **processB** in order to develop your solution.
-
-## *libbitmap* installation and usage
 To work with the bitmap library, you need to follow these steps:
 1. Download the source code from [this GitHub repo](https://github.com/draekko/libbitmap.git) in your file system.
 2. Navigate to the root directory of the downloaded repo and run the configuration through command ```./configure```. Configuration might take a while.  While running, it prints some messages telling which features it is checking for.
@@ -17,15 +18,8 @@ To work with the bitmap library, you need to follow these steps:
 5. Upon completing the installation, check that the files have been properly installed by navigating to ```/usr/local/lib```, where you should find the ```libbmp.so``` shared library ready for use.
 6. In order to properly compile programs which use the *libbitmap* library, you first need to notify the **linker** about the location of the shared library. To do that, you can simply add the following line at the end of your ```.bashrc``` file:      
 ```export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"```
-### Using *libbitmap* in your code
-Now that you have properly installed the library in your system, it's time to use it in your programs:
-1. Include the library in your programs via ```#include <bmpfile.h>```. If you want to check the content of ```bmpfile.h``` to glimpse the functionalities of the library, navigate to ```/usr/local/include```, where the header file should be located.
-2. Compile programs which use the *libbitmap* library by linking the shared library with the ```-lbmp``` command.     
-Example for compiling **circle.c**: ```gcc src/circle.c -lbmp -lm -o bin/circle``` 
 
-## Compiling and running **processA** and **processB**
-The two processes are implemented as UIs through *ncurses* library, therefore you need to compile their source files by linking the shared library via ```-lncurses```. As for the first assignment, exploit the resize event of the windows to get out of situations in which the graphical elements do not properly spawn.
-
-## Executing **circle.c**
-This is a simple example of a program which uses the *libbitmap* library. It generates a 100x100 colored `.bmp` file with user-defined name, depicting a blue circle of given radius. When you execute it, pass the two arguments (file name and radius value) along. Execution example: ```./bin/circle out/test.bmp 20```.
-
+### Use of the *libbitmap* library in our programs
+To use it in our programs:
+1. include the library in your programs via ```#include <bmpfile.h>```. If you want to check the content of ```bmpfile.h``` to glimpse the functionalities of the library, navigate to ```/usr/local/include```, where the header file should be located.
+2. compile programs which use the *libbitmap* library by linking the shared library with the ```-lbmp``` command.     
--- ARP-SecondAssignment/src/processB.c
+++ ARP-SecondAssignment/src/processB.c
@@ -19,7 +19,7 @@ const int depth = 4;
 // Define struct for shared memory and variables
 struct shared
 {
-    int m[1600][600];
+    int m[1600][600];   // Matrix for the shared memory
 };
 
 //const char * sem_fn = { "my_sem"};
@@ -40,8 +40,8 @@ void CheckCorrectness(int c)
 {
     if(c == -1) 
     {
-        close(log_fd);
-        perror("Error in writing function");
+        close(log_fd);  // Close the log file
+        perror("Error in writing function");    // Print the error message
         exit(1);
     }
 }
@@ -49,37 +49,37 @@ void CheckCorrectness(int c)
 // Function to draw a blue circle
 void draw_blue_circle(int radius,int x,int y, bmpfile_t *bmp) {
 
-    rgb_pixel_t pixel = {255, 0, 0, 0};
+    rgb_pixel_t pixel = {255, 0, 0, 0}; // Blue color
     for(int i = -radius; i <= radius; i++) {
-    for(int j = -radius; j <= radius; j++) {
-      // If distance is smaller, point is within the circle
-      if(sqrt(i*i + j*j) < radius) {
-          /*
-          * Color the pixel at the specified (x,y) position
-          * with the given pixel values
-          */
-          bmp_set_pixel(bmp, x*20 + i, y*20 + j, pixel);
-      }
+        for(int j = -radius; j <= radius; j++) {
+            // If distance is smaller, point is within the circle
+            if(sqrt(i*i + j*j) < radius) {
+                /*
+                * Color the pixel at the specified (x,y) position
+                * with the given pixel values
+                */
+                bmp_set_pixel(bmp, x*20 + i, y*20 + j, pixel);  // Draw the circle
+            }
+        }
     }
-  }
 }
 
 // Function to cancel the blue circle
 void cancel_blue_circle(int radius,int x,int y, bmpfile_t *bmp) {
 
-    rgb_pixel_t pixel = {255, 255, 255, 0};
+    rgb_pixel_t pixel = {255, 255, 255, 0}; // White color
     for(int i = -radius; i <= radius; i++) {
-    for(int j = -radius; j <= radius; j++) {
-      // If distance is smaller, point is within the circle
-      if(sqrt(i*i + j*j) < radius) {
-          /*
-          * Color the pixel at the specified (x,y) position
-          * with the given pixel values
-          */
-          bmp_set_pixel(bmp,  x*20+i,y*20+  j, pixel);
-      }
+        for(int j = -radius; j <= radius; j++) {
+            // If distance is smaller, point is within the circle
+            if(sqrt(i*i + j*j) < radius) {
+                /*
+                * Color the pixel at the specified (x,y) position
+                * with the given pixel values
+                */
+                bmp_set_pixel(bmp,  x*20+i,y*20+  j, pixel);    // Cancel the circle
+            }
+        }
     }
-  }
 }
 
 int main(int argc, char const *argv[])
@@ -88,7 +88,7 @@ int main(int argc, char const *argv[])
     if ((log_fd = open("processB.log",O_WRONLY|O_APPEND|O_CREAT, 0666)) == -1)
     {
         // If the file could not be opened, print an error message and exit
-        perror("Error opening command file");
+        perror("Error opening command file");   // Print the error message
         exit(1);
     }
 
@@ -107,48 +107,47 @@ int main(int argc, char const *argv[])
 
 
 
-    ShmKEY = ftok(".", 'x');
-    ShmID = shmget(ShmKEY, sizeof(struct shared), 0666);
+    ShmKEY = ftok(".", 'x');    // Get the key of the shared memory
+    ShmID = shmget(ShmKEY, sizeof(struct shared), 0666);    // Get the ID of the shared memory
 
     if (ShmID < 0) {
-    printf("*** shmget error (server) ***\n");
-    exit(1);
+        printf("*** shmget error (server) ***\n");  // If the ID is not correct, print an error message
+        exit(1);
     }
 
-
-    ShmPTR = (struct shared *) shmat(ShmID, NULL, 0);
+    ShmPTR = (struct shared *) shmat(ShmID, NULL, 0);   // Attach the shared memory to the pointer
 
     if ((int) ShmPTR == -1) {
-    printf("*** shmat error (server) ***\n");
-    exit(1);
+        printf("*** shmat error (server) ***\n");   // If the pointer is not correct, print an error message
+        exit(1);
     }
 
     bmpfile_t *bmp;
     bmp = bmp_create(width, height, depth);
 
-    int center_cord = 0;
-    int y_cord[600];
-    int x_cord[600];
-    int y;
+    int center_cord = 0;    // Variable to store the center of the circle
+    int y_cord[600];    // Array to store the y coordinates of the circle
+    int x_cord[600];    // Array to store the x coordinates of the circle
+    int y;  
     int flag ;
     int old_x;
     int old_y;
 
-    int radius = 30;
+    int radius = 30;    // Radius of the circle
 
-    semaphore = sem_open("/mysem", 0);
-    semaphore2 = sem_open("/mysem2", 0);
+    semaphore = sem_open("/mysem", 0);      // Open the semaphore
+    semaphore2 = sem_open("/mysem2", 0);    // Open the semaphore
 
 
     if(semaphore == (void*) -1)
     {
-        perror("sem_open failure");
+        perror("sem_open failure"); // If the semaphore is not correct, print an error message
         exit(1);
     }
 
     if(semaphore2 == (void*) -1)
     {
-        perror("sem_open failure");
+        perror("sem_open failure"); // If the semaphore is not correct, print an error message
         exit(1);
     }
 
@@ -171,105 +170,98 @@ int main(int argc, char const *argv[])
         // If user resizes screen, re-draw UI...
         if(cmd == KEY_RESIZE) {
             if(first_resize) {
-                first_resize = FALSE;
+                first_resize = FALSE;   // Avoid trigger resize event on launch
             }
             else {
-                reset_console_ui();
+                reset_console_ui(); // Re-draw UI
             }
         }
 
         else
         {
 
-        mvaddch(LINES/2, COLS/2, '0');
+            mvaddch(LINES/2, COLS/2, '0');  // Draw the center of the circle
 
-        refresh();
+            refresh();  // Refresh the screen
 
-        sem_wait(semaphore2);
+            sem_wait(semaphore2);   // Wait the semaphore
 
-        //old center
-        if(y_cord[y-1] != 0 && x_cord[y-1] != 0)
-        {
-            old_x = x_cord[y-1];
-            old_y = y_cord[y-1];
-        }
-        
-        for (int i=0;i<600;i++)
-        {
-            if(y_cord[i] != 0)
+            //old center
+            if(y_cord[y-1] != 0 && x_cord[y-1] != 0)
             {
-                y_cord[i]=0;
+                old_x = x_cord[y-1];    // Get the old x coordinate
+                old_y = y_cord[y-1];    // Get the old y coordinate
             }
-            else if(x_cord[i] != 0)
-            {
-                x_cord[i]=0;
-            }
-        
-        }
-
-        center_cord = 0;
-
-    
-        int i, j;
-        y = 0;
-        flag = 0;
-        
-        //algorithm to find the center of the circle
-        for (i = 0; i < 1600; i++) {
             
-            if (flag == 1) {
-                
-                break;
-            }
-            for (j = 0; j < 600; j++) {
-                if (ShmPTR->m[i][j] == 1)
+            for (int i=0;i<600;i++)
+            {
+                if(y_cord[i] != 0)
                 {
-                    y_cord[y] = j;
-                    x_cord[y] = i;
+                    y_cord[i]=0;    // Reset the y coordinates
+                }
+                else if(x_cord[i] != 0)
+                {
+                    x_cord[i]=0;    // Reset the x coordinates
+                }
+            
+            }
 
-                    if (y_cord[y] > y_cord[y-1]) {
+            center_cord = 0;    // Reset the center of the circle
+        
+            int i, j;
+            y = 0;
+            flag = 0;
+            
+            //algorithm to find the center of the circle
+            for (i = 0; i < 1600; i++) {
+                
+                if (flag == 1) {
+                    break;
+                }
 
-                        flag = 1;
+                for (j = 0; j < 600; j++) {
+                    if (ShmPTR->m[i][j] == 1)
+                    {
+                        y_cord[y] = j;  // Store the y coordinate
+                        x_cord[y] = i;  // Store the x coordinate
+
+                        // If the y coordinate is greater than the previous one, break the loop
+                        if (y_cord[y] > y_cord[y-1]) {
+                            flag = 1;
+                            break;
+                        }
+
+                        y++;    // Increment the y coordinate
                         break;
                     }
-
-                    y++;
-                    break;
-                
-
                 }
             }
+
+            sprintf(log_buffer, "<Process_B> Position of center updated: %s\n", asctime(info));
+            check = write(log_fd, log_buffer, strlen(log_buffer));  // Write the log message
+            CheckCorrectness(check);    // Check if the write is correct
+
+            mvaddch(floor((int)((y_cord[y-1]+radius)/20)),floor((int)(x_cord[y-1]/20)), '0');   
+
+            refresh();
+
+            sem_post(semaphore);
+
+            
+            //update the position of the blue circle
+            cancel_blue_circle(radius,old_x,old_y,bmp); // Cancel the old blue circle
+
+            draw_blue_circle(radius,x_cord[y-1],y_cord[y-1],bmp);  // Draw the new blue circle
         }
-
-        
-
-        sprintf(log_buffer, "<Process_B> Position of center updated: %s\n", asctime(info));
-        check = write(log_fd, log_buffer, strlen(log_buffer));
-        CheckCorrectness(check);
-
-        mvaddch(floor((int)((y_cord[y-1]+radius)/20)),floor((int)(x_cord[y-1]/20)), '0');
-
-        refresh();
-
-        sem_post(semaphore);
-
-        
-        //update the position of the blue circle
-        cancel_blue_circle(radius,old_x,old_y,bmp);
-
-        draw_blue_circle(radius,x_cord[y-1],y_cord[y-1],bmp);  
-   
-        
-        }
-         
     }
-    sem_close(semaphore);
-    sem_close(semaphore2);
 
-    shmdt((void *) ShmPTR);
-    bmp_destroy(bmp);
+    sem_close(semaphore);   // Close the semaphore
+    sem_close(semaphore2);  // Close the semaphore
 
-    endwin();
+    shmdt((void *) ShmPTR); // Detach the shared memory
+    bmp_destroy(bmp);       // Destroy the bitmap
+
+    endwin();   // End the window
 
     // Close the log file
     close(log_fd);
